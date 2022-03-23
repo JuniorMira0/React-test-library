0d81f2a8ed8be8ecef6f344fb7bfa98076cbae1e
JuniorMira <j-unyor14@hotmail.com>
2022-03-23 17:12:28 -0300

    requirement 3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRoter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  renderWithRoter(<NotFound />);
+  it('Teste se página contém um texto Page requested not found', () => {
+    const headingH2 = screen.getByText(/Page requested not found/i);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', {
+      name: /Pikachu crying because the page/i,
+    });
+
+    expect(img).toHaveAttribute('src', url);
+    expect(headingH2).toBeInTheDocument();
+  });
+});
